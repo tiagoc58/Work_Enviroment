4cd3ed5490b514ffd903e43fa0b9732fa9244984
tiagoc58 <tiagocorredor0508@gmail.com>
2025-09-04 06:53:09 -0500

    Se agrega lg2 para long long

--- cpp/plantilla.cpp
+++ cpp/plantilla.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define Sonic ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define tests(t) int t; cin >> t; while(t--)
-#define F first 
+#define F first
 #define S second
 #define pb push_back
 #define ln cout<<endl
@@ -27,17 +27,17 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int,int> pii;
 typedef pair<int,pii> piii;
-typedef pair<ll,ll> pll; 
+typedef pair<ll,ll> pll;
 ll gcd(ll a, ll b){while(b){a%=b; swap(a,b);} return a;} ll lcm(ll a,ll b){return a*b/gcd(a,b);}
-int lg2(const int &x) { return 31-__builtin_clz(x);}
- 
+int lg2(const int &x) { return 31-__builtin_clz(x);} // int lg2(const ll &x) {return 63-__builtin_clzll(x);}
+
 void solve(){
     
 }
- 
+
 int main(){
     Sonic
-    tests(t)
+    // tests(t)
         solve();
     return 0;
 }
